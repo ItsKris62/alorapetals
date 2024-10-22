6a72faa775263f393a523e1c3722cd784e8e6bcf
Christopher Rateng <44862569+ItsKris62@users.noreply.github.com>
2024-10-22 11:23:26 +0000

    Changed the hero section from using an image to a video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,28 @@
-import flowerImage from "../assets/images/greenhouse.jpg";
+import flowerVideo from "../assets/images/greenhouse.jpg";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <section className="relative h-screen w-full">
-      {/* Image Background */}
-      <img
+      {/* Video Background */}
+      <video
         className="absolute top-0 left-0 w-full h-full object-cover"
-        src={flowerImage}
-        alt="Greenhouse"
-      />
+        src={flowerVideo}
+        autoPlay
+        loop
+        muted
+      ></video>
       {/* Gradient Overlay */}
       <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent h-full"></div>
       {/* Content Centered */}
       <div className="absolute bottom-0 left-0 w-full h-1/4 bg-gradient-to-t from-black to-transparent flex flex-col justify-center items-center pb-10">
         {/* Text Above Button */}
-        <h2 className="font-oswald text-white text-3xl md:text-4xlfont-bold text-center mb-4">
+        <h2 className="font-oswald text-white text-2xl md:text-3xl lg:text-4xl font-semibold text-center mb-4">
           Discover the beauty of our premium flowers
         </h2>
         {/* Call to Action */}
         <Link to="/catalog">
+          {/* Use Link if using React Router */}
           <button className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-purple-600 text-white font-bold py-3 px-6 rounded-full shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
             Explore Our Catalog
           </button>
